397b31e083b120d1bdb5eb45b072485ca7808153
Muhammad Al-Ali <mhmdalrab6@gmail.com>
2026-05-05 19:24:21 +0300

    Delete backend directory

--- backend/src/main/java/com/NGLP/backend/repository/CourseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.NGLP.backend.repository;
-
-import com.NGLP.backend.entity.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CourseRepository extends JpaRepository<Course, Long> {ا
-}
